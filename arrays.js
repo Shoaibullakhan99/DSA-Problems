b9f9e57e83cd507af598a45b22a665d9df424c66
Shoaib ulla khan <shoaibullakhan1999@outlook.com>
2024-05-17 15:15:32 +0530

    i

--- arrays.js
+++ arrays.js
@@ -24,31 +24,3 @@ function findPair(nums, target){
 
 findPair(nums, target)
 
-// Using Two Pointer approach
-
-// First sort the array in ascending order 
-// Point low at beginning and high at end
-// add low and high , if target is less than sum shift high pointer to left
-//  if target is greater than sum , shift low pointer to right
-// if equal log the values.
-// Do this untill left pointer if les than right pointer.
-
-
-function findPairUsingPointers(nums, target){
-    nums.sort((a,b) => a - b)
-    let left = 0;
-    let right = nums.length - 1;
-    while(left < right){
-        if(nums[left] + nums[right] == target){
-            console.log(nums[left], nums[right]);
-            right --;
-            left ++;
-        } else if(nums[left] + nums[right] > target){
-            right--;
-        } else {
-            left ++
-        }
-    }
-}
-
-findPairUsingPointers(nums, target)
