098f12a5f39c230a8a498eb0d2ac168fc821a950
Shoaib ulla khan <shoaibullakhan1999@outlook.com>
2024-05-20 17:42:38 +0530

    tried better approach for sorting o's 1's and 2's

--- sortArrays.js
+++ sortArrays.js
@@ -7,3 +7,22 @@ function sortArray(arr){
 
 sortArray( [2,0,2,1,1,0]);
 
+// Better approach
+
+function sortBetter(arr){
+    var ones = 0, twos = 0, zeros = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == 0){
+            zeros += 1;
+        } else if(arr[i] == 1){
+            ones += 1;
+        } else if(arr[i] == 2) {
+            twos += 1;
+        }
+    }
+       for(let i = 0; i < arr.length; i++){
+            while( zeros > 0){
+                arr[i] = 0;
+            }
+       } 
+}
